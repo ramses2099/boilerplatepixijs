f6ae74c8f71629c5da3c670c0f1d144fc45c371f
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2022-10-26 15:18:46 -0400

    last commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import SpriteUtil from "./js/spriteUtil.js";
-
 ///////////////////////////////////////////////////////////
 (function (window) {
   window.addEventListener("load", function () {
@@ -12,11 +10,11 @@ import SpriteUtil from "./js/spriteUtil.js";
   });
 
   const Application = PIXI.Application,
+    Rectangle = PIXI.Rectangle,
     loader = PIXI.Loader.shared,
     resources = loader.resources,
     Sprite = PIXI.Sprite,
     b = new Bump(PIXI);
-    
 
   const CANVAS_WIDTH = 600;
   const CANVAS_HEIGHT = 500;
@@ -30,23 +28,10 @@ import SpriteUtil from "./js/spriteUtil.js";
   });
 
   document.body.appendChild(app.view);
-  /*--
-  let renderer = PIXI.autoDetectRenderer(CANVAS_WIDTH, CANVAS_HEIGHT);
-  document.body.appendChild(renderer.view);
-  let stage = new Container();
-  
-  var smoothie = new Smoothie({
-      engine: "pixi", 
-      renderer: renderer,
-      root: stage,
-      fps: 30,
-      update: update.bind(this)
-    });
-    --*/
 
   let renderer = app.renderer;
   let stage = app.stage;
- 
+
   var smoothie = new Smoothie({
     engine: "pixi",
     renderer: renderer,
@@ -55,44 +40,202 @@ import SpriteUtil from "./js/spriteUtil.js";
     update: update.bind(this),
   });
 
-  let sprite;
-
   loader.add("spritesheet", "./img/spritesheet.json").load(setup);
+
+  class Platform {
+    constructor(x, y, width = 32, height = 32) {
+      this.graphics = new PIXI.Graphics();
+      this.position = new PIXI.Point(x, y);
+      this.velocity = new PIXI.Point(0, 0);
+      this.gravity = 0.3;
+      this.friction = 0.9;
+      this.rect = new Rectangle(
+        this.position.x,
+        this.position.y,
+        width,
+        height
+      );
+    }
+    //
+    Draw(stage) {
+      this.graphics.clear();
+      this.graphics.lineStyle(1, 0xffbd01, 1);
+      this.graphics.beginFill(0xc34288);
+      this.graphics.drawRect(
+        this.rect.x,
+        this.rect.y,
+        this.rect.width,
+        this.rect.height
+      );
+      this.graphics.endFill();
+      stage.addChild(this.graphics);
+    }
+    //
+    Update() {
+      this.velocity.y += this.gravity;
+
+      //friction
+      this.velocity.x += this.friction;
+      this.velocity.y += this.friction;
+
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+
+      //this.rect.x += this.position.x;
+      this.rect.y = this.position.y;
+      this.velocity.y = 0;
+
+      //console.log(`rect x: ${this.rect.x}, y: ${this.rect.y}`);
+    }
+  }
+  //
+  class Rect {
+    constructor(l, t, w = 32, h = 32) {
+      this.graphics = new PIXI.Graphics();
+      this.l = this.ol = l; //left and old left
+      this.r = this.or = l + w; //right and old right
+      this.t = this.ot = t; //top and old bottom
+      this.b = this.ob = t + h; //bottom and old bottom
+
+      this.vx = this.vy = 0;
+
+      this.h = h;
+      this.w = w;
+    }
+    //
+    setBottom(b) {
+      this.b = b;
+      this.t = b - this.w;
+    }
+    setLeft(l) {
+      this.l = l;
+      this.r = l + this.w;
+    }
+    setRight(r) {
+      this.r = r;
+      this.l = r - this.w;
+    }
+    setTop(t) {
+      this.t = t;
+      this.b = t + this.h;
+    }
+    //
+  }
+  //
+  class Plat extends Rect {
+    constructor(l, t, w = 32, h = 32) {
+      super(l, t, w, h);
+
+      this.d = Math.random() * Math.PI * 2;
+      this.rotation = Math.random() * Math.PI * 2;
+    }
+    //
+    Draw(stage) {
+      this.graphics.clear();
+      this.graphics.lineStyle(1, 0xffbd01, 1);
+      this.graphics.beginFill(0xc34288);
+      this.graphics.drawRect(
+        this.l,
+        this.t,
+        this.w,
+        this.h
+      );
+      this.graphics.endFill();
+      stage.addChild(this.graphics);
+    }
+    //
+    collideBoundaries(l, r, t, b){
+        if(this.l < l){
+            this.d = Math.atan2(Math.sin(this.d), Math.cos(this.d) * -1);
+            this.vx *= -1;
+            this.setLeft(l);
+        }else if(this.r > r){
+            this.d = Math.atan2(Math.sin(this.d), Math.cos(this.d) * -1);
+            this.vx *= -1;
+            this.setRight(r)
+        }
+
+        if(this.t <= t){
+            this.d = Math.atan2(Math.sin(this.d) * -1, Math.cos(this.d));
+            this.vy *= -1;
+            this.setTop(t);
+        }else if(this.b > b){
+            this.d = Math.atan2(Math.sin(this.d) * -1, Math.cos(this.d));
+            this.vy *= -1;
+            this.setBottom(b);
+        }
+
+    }
+    //
+    Update() {
+        this.vx = Math.cos(this.d);
+        this.vy = Math.sin(this.d);
+        //
+        this.rotation += 0.05;
+        this.vy += Math.sin(this.rotation);
+
+        this.ob = this.b;
+        this.ol = this.l;
+        this.or = this.r;
+        this.ot = this.t;
+        //
+        this.l += this.vx;
+        this.t += this.vy;
+        this.r = this.l + this.w;
+        this.b = this.t + this.h;
+
+    }
+  }
+
+  const platforms = [];
+
+  //fill
+  for (let i = 0; i < 20; i++) {
+    let l = Math.random() * CANVAS_WIDTH - i;
+    let t = Math.random() * CANVAS_HEIGHT - i;
+    let w = Math.random() * 10 * i;
+    let h = 16;
+    
+    platforms.push(new Plat(l,t,w,h));    
+  }
+
+  //COLLIDE
+  function collideFloor(obj) {
+    if (obj.rect.bottom > CANVAS_HEIGHT - obj.rect.height) {
+      obj.position.y = CANVAS_HEIGHT - obj.rect.height;
+    }
+  }
+
+  let p1 = new Platform(CANVAS_WIDTH / 2, 150);
+
+  let b1 = new Plat(250, 16, 120, 16);
 
   function setup() {
     let textures = resources["spritesheet"].textures;
 
-    sprite = new Sprite(textures["ship_A.png"]);
-    sprite.anchor.set(0.5);
-    sprite.x = Math.random() * CANVAS_WIDTH;
-    sprite.y = (Math.random() * CANVAS_HEIGHT) / 2;
-    sprite.rotation = Math.PI;;
-
-    stage.addChild(sprite);
-
-    const spriteUtil = new SpriteUtil(PIXI);
-
-    let sp = spriteUtil.sprite(textures['meteor_large.png'], 250,150);
-    stage.addChild(sp);
-    
-    let rect = spriteUtil.rectangle(25, 25);
-
-    stage.addChild(rect);
-
-    let c = spriteUtil.circle(CANVAS_WIDTH/2, 45, 30, '0xffffff', '0xFFBD01', 3);
-    stage.addChild(c);
-
-    
-
-
     smoothie.start();
   }
-
+  //
   function update() {
-    sprite.y += 1;
+    p1.Update();
+
+    collideFloor(p1);
+
+    p1.Draw(stage);
     //
-    if (b.hit({ x: sprite.x, y: CANVAS_HEIGHT }, sprite)) {
-      sprite.y = CANVAS_HEIGHT - 16;
-    }
+    b1.Update();
+    b1.Draw(stage);
+    
+    //
+    platforms.forEach(t => {
+        t.Update();
+
+
+        t.collideBoundaries(0, CANVAS_WIDTH, 0, (CANVAS_HEIGHT-t.h));
+
+        t.Draw(stage);
+    });
+
+
   }
 })(window);
